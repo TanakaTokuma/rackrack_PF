4d79569650b665857f7f3301d52738174fe37018
TanakaTokuma <tt19940711@gmail.com>
2022-07-16 06:41:27 +0000

    add active_storage

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -1,2 +1,26 @@
 class Public::BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+  
+  
+  
+  
+  
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :author, :publisher, :saledate, :caption)
+  end
+  
+  
 end
